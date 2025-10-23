5502c047ded0d8df3417c50b5b3d6873284c9f60
Alisson-7-Neres <alisson.neres@outlook.com.br>
2025-10-23 17:41:35 -0300

    Parametrizando comandos SQL e chamando Procedures

---
+++ src/main/java/com/dio/entity/EmployeeParamDAO.java
@@ -0,0 +1,146 @@
+package com.dio.entity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimeZone;
+
+import com.dio.persistence.ConnectionUtil;
+import com.mysql.cj.jdbc.StatementImpl;
+
+public class EmployeeParamDAO {
+	
+	public void insert(final EmployeeEntity employeeEntity) {
+		try {
+			Connection connection = ConnectionUtil.getConnection(); // Criando conexão
+			PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employees (name,salary, birthday) VALUES (? , ? , ?)");
+			
+			preparedStatement.setString(1, employeeEntity.getName());
+			preparedStatement.setBigDecimal(2, employeeEntity.getSalary());
+			preparedStatement.setTimestamp(3, Timestamp.valueOf(employeeEntity.getBithday().atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime()));
+			preparedStatement.executeUpdate();
+			if (preparedStatement instanceof StatementImpl impl)
+				employeeEntity.setId(impl.getLastInsertID());
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void insertWithProcedure(final EmployeeEntity employeeEntity) {
+		try {
+			Connection connection = ConnectionUtil.getConnection(); // Criando conexão
+			java.sql.CallableStatement statement = connection.prepareCall
+					("CALL prc_insert_employee(?, ?, ?, ?);");
+			
+			statement.registerOutParameter(1, TimeZone.LONG); 
+			statement.setString(2, employeeEntity.getName());
+			statement.setBigDecimal(3, employeeEntity.getSalary());
+			statement.setTimestamp(4, Timestamp.valueOf(employeeEntity.getBithday().atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime()));
+			statement.execute();
+			employeeEntity.setId(statement.getLong(1));
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+	public void update(final EmployeeEntity employeeEntity) {
+		try {
+			Connection connection = ConnectionUtil.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(""
+					+ "UPDATE employees SET name =?, salary =?, birthday =? WHERE id =?");
+			
+			preparedStatement.setString(1, employeeEntity.getName());
+			preparedStatement.setBigDecimal(2, employeeEntity.getSalary());
+			preparedStatement.setTimestamp(3, Timestamp.valueOf(employeeEntity.getBithday().atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime()));
+			preparedStatement.setLong(4, employeeEntity.getId());
+			
+			preparedStatement.executeUpdate();
+
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+			}
+	}
+
+	public void delete(final long id) {		
+		try {
+		Connection connection = ConnectionUtil.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatement(""
+				+ "DELETE FROM employees WHERE id =?");
+		
+		preparedStatement.setLong(1, id);
+		
+		preparedStatement.execute();
+		findAll();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public List<EmployeeEntity> findAll() {
+		List<EmployeeEntity> entities = new ArrayList<>();
+		try {
+			Connection connection = ConnectionUtil.getConnection();
+			Statement statement = connection.createStatement();
+			
+			String sql = 
+			"SELECT * FROM employees;";
+			statement.executeQuery(sql);
+			ResultSet resultSet = statement.getResultSet(); // Guarda o resultado de uma consulta SQL (SELECT)
+			while(resultSet.next()) { // Enquanto tiver resultados
+				var entity = new EmployeeEntity();
+				entity.setId(resultSet.getLong("id"));
+				entity.setName(resultSet.getString("name"));
+				entity.setSalary(resultSet.getBigDecimal("salary"));
+				Instant birthdayInstant = resultSet.getTimestamp("birthday").toInstant();
+				entity.setBithday(OffsetDateTime.ofInstant(birthdayInstant, ZoneOffset.UTC));
+				entities.add(entity);
+				System.out.println(entity);
+				}
+			} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+
+	public EmployeeEntity findById(final long id) {
+		EmployeeEntity entity = new EmployeeEntity();
+		try {
+			Connection connection = ConnectionUtil.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(""
+					+ "SELECT * FROM employees HWERE id =?");
+			
+			ResultSet resultSet = preparedStatement.getResultSet();
+			
+			preparedStatement.setLong(1, id);
+			preparedStatement.executeQuery();
+			
+			if (resultSet.next()) { // Como só vai exibir só uma usuário, usa-se a condicional ao inves do laço de repetição
+			entity.setId(resultSet.getLong("id"));
+			entity.setName(resultSet.getString("name"));
+			entity.setSalary(resultSet.getBigDecimal("salary"));
+			Instant birthdayInstant = resultSet.getTimestamp("birthday").toInstant();
+			entity.setBithday(OffsetDateTime.ofInstant(birthdayInstant, ZoneOffset.UTC));
+			System.out.println(entity);
+			} else
+				System.err.println("ID não encontrado!");
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	private String formatOffsetDateTime(final OffsetDateTime dateTime) {
+		Object utdcDateTime = dateTime.withOffsetSameInstant(ZoneOffset.UTC); // Converte o horário para o fuso UTC, mantendo o instante exato do tempo
+		return dateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd  HH:mm:ss"));
+	}
+
+}
--- src/main/java/com/dio/main/Main.java
+++ src/main/java/com/dio/main/Main.java
@@ -8,10 +8,11 @@ import org.flywaydb.core.Flyway;
 import com.dio.entity.EmployeeAuditDAO;
 import com.dio.entity.EmployeeDAO;
 import com.dio.entity.EmployeeEntity;
+import com.dio.entity.EmployeeParamDAO;
 
 public class Main {
 	
-	private final static EmployeeDAO employeeDao = new EmployeeDAO();
+	private final static EmployeeParamDAO employeeParamDao = new EmployeeParamDAO();
 	private final static EmployeeAuditDAO employeeAuditDAO =  new EmployeeAuditDAO();
 
 	public static void main(String[] args) {
@@ -21,32 +22,59 @@ public class Main {
 				.load();
 		//flyway.repair();
 		flyway.migrate();
-		
 		/*
-		var employee = new EmployeeEntity();
-		employee.setName("Miguel Da Silva");
-		employee.setSalary(new BigDecimal("3200"));
-		employee.setBithday(OffsetDateTime.now().minusYears(23));
-		System.out.println(employee);
-		employeeDao.insert(employee);
-		System.out.println(employee);
-		 */
+		var employeeMiguel = new EmployeeEntity();
+		employeeMiguel.setName("Miguel Da Silva");
+		employeeMiguel.setSalary(new BigDecimal("3200"));
+		employeeMiguel.setBithday(OffsetDateTime.now().minusYears(23));
+		System.out.println(employeeMiguel);
+		employeeParamDao.insert(employeeMiguel);
+		System.out.println(employeeMiguel);
 		
-		// employeeDao.findAll();
 		
-		//employeeDao.findById(2);
+		var employeeRenato = new EmployeeEntity();
+		employeeRenato.setName("Renato Tavares Almeida");
+		employeeRenato.setSalary(new BigDecimal("2500"));
+		employeeRenato.setBithday(OffsetDateTime.now().minusYears(32));
+		System.out.println(employeeRenato);
+		employeeParamDao.insert(employeeRenato);
 		
-		/*
-		var employee = new EmployeeEntity();
-		employee.setId(1);
-		employee.setName("Alisson Neres Ribeiro");
-		employee.setSalary(new BigDecimal("2500"));
-		employee.setBithday(OffsetDateTime.now().minusYears(32));
-		System.out.println(employee);
-		employeeDao.update(employee);
+		var employeeJessica = new EmployeeEntity();
+		employeeJessica.setName("Jessica Henrique Chagas");
+		employeeJessica.setSalary(new BigDecimal("4200"));
+		employeeJessica.setBithday(OffsetDateTime.now().minusYears(27));
+		System.out.println(employeeJessica);
+		employeeParamDao.insert(employeeJessica);
+		
+		var employeeAlbert = new EmployeeEntity();
+		employeeAlbert.setName("Albert Sneijder Lopez");
+		employeeAlbert.setSalary(new BigDecimal("1500"));
+		employeeAlbert.setBithday(OffsetDateTime.now().minusYears(38));
+		System.out.println(employeeAlbert);
+		employeeParamDao.insert(employeeAlbert);
+		
+		
+		var employeeAndre = new EmployeeEntity();
+		employeeAndre.setName("André Pereira Santos");
+		employeeAndre.setSalary(new BigDecimal("1900"));
+		employeeAndre.setBithday(OffsetDateTime.now().minusYears(18));
+		System.out.println(employeeAndre);
+		employeeParamDao.insert(employeeAndre);
 		*/
 		
-		// employeeDao.delete(5);
+		var employeeAndrea = new EmployeeEntity();
+		employeeAndrea.setName("Andrea Freire");
+		employeeAndrea.setSalary(new BigDecimal("2900"));
+		employeeAndrea.setBithday(OffsetDateTime.now().minusYears(22));
+		System.out.println(employeeAndrea);
+		employeeParamDao.insertWithProcedure(employeeAndrea);
+		
+		
+		//employeeParamDao.findAll();
+		
+		//employeeParamDao.findById(2);
+		
+		// employeeParamDao.delete(2);
 		
 		//employeeAuditDAO.findAll().forEach(System.out::println);
 	}
